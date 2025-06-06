ed740d927ce9d15c1aa18d9aa538006e6b14f946
Kevin McAleer <kevinmcaleer@gmail.com>
2025-06-06 13:04:00 +0100

    added file checking

--- install.sh
+++ install.sh
@@ -20,5 +20,9 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 
 echo "Creating virtual environment and installing dependencies..."
+if [ ! -d "venv" ]; then
 uv venv venv
+else
+  echo "Virtual environment already exists."
+fi
 uv pip install -r requirements.txt
