5b6b3945882b25f1a62ddf61fb4666247c40bcf3
Kevin McAleer <kevinmcaleer@gmail.com>
2025-06-06 11:34:15 +0100

    updated installer

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 !/bin/bash
+# if not exists:
+if [ ! -d "vosk-model-small-en-us-0.15" ]; then
+  echo "Vosk model directory does not exist. Downloading..."
+else
+  echo "Vosk model directory already exists."
+  exit 0
+fi
 wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
 unzip vosk-model-small-en-us-0.15.zip
 
 echo "Vosk model downloaded and extracted."
 echo "installong PortAudio dependencies..."
 sudo apt update
-sudo apt install libportaudio2 portaudio19-dev
+sudo apt install libportaudio2 portaudio19-dev -y
