5aa44c3a345dcc64ffc34d734e537f5aa4f55ea5
Kevin McAleer <kevinmcaleer@gmail.com>
2025-06-06 11:35:28 +0100

    updated installer

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # if not exists:
 if [ ! -d "vosk-model-small-en-us-0.15" ]; then
   echo "Vosk model directory does not exist. Downloading..."
+  wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
+  unzip vosk-model-small-en-us-0.15.zip
 else
   echo "Vosk model directory already exists."
-  exit 0
+
 fi
-wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
-unzip vosk-model-small-en-us-0.15.zip
 
 echo "Vosk model downloaded and extracted."
 echo "installong PortAudio dependencies..."
